c0f078420775d3dfc2c7343fa4416e1e852b0ba7
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-28 22:26:49 -0300

    Funcion de ventana principal

--- main.js
+++ main.js
@@ -7,3 +7,22 @@ const DB =  require('./db') // Modulo para manejar sqlite
 let mainWindow ; // Ventana principal
 let views = []; //array de vistas (pestañas)
 let activeIndex = 0 ; // indice de la vista activa
+
+// Funcion para crear la ventana principal
+function createWindow(){
+    mainWindow = new BrowserWindow({
+        width: 1200,  // ancho nicial
+        height: 800, // alto inicial
+        webPreferences{
+            preload: path.join(__dirname, 'preload.js'), // carga preload.js  para exponer API segura
+            contexIsolation: true , // Aisla contexto (seguridad)
+            nodeIntegratio: false , // evita node en renderer (seguridad)
+            sanbox: true // habilita sandbox (seguridad)
+        }
+    });
+
+
+
+
+
+}
