1b914b580408edb8906f36375f83f05db6676193
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-29 11:35:42 -0300

    Guardamos datos de la pestana y fijamos los tamanos de la browser view

--- main.js
+++ main.js
@@ -2,7 +2,8 @@
 // main.js  - Proceso principal de  Electron 
 const { app, BrowserWindow, BrowserView, ipcMAin } = require('electron'); // Trae mdodulos de Electron
 const path = require('path'); // Modulo para manejar rutas de archivos 
-const DB =  require('./db') // Modulo para manejar sqlite
+const DB =  require('./db'); // Modulo para manejar sqlite
+const { title } = require('process');
 
 let mainWindow ; // Ventana principal
 let views = []; //array de vistas (pestañas)
@@ -37,4 +38,28 @@ function createTab(url = 'https://www.google.com'){
             sainbox: true
         }
     });
-}
+    //Guarda metadatos de la pestana 
+    views.push({view, url});
+
+    // anade la vista a la ventana principal
+    mainWindow.addBrowserView(view);
+
+    // Fija la posicion  y tamaño del browser view debajo  de la barra de navegacion (y 40 px  de alto  para la barra )
+    const [w,h] = mainWindow.getContentSize();
+    view.setBounds({x: 0 , y: 40,  width: w, height: h -40});
+    view.setAutoResize({width: true, height:true});
+
+    // Carga la URL inicial 
+    view.webContents.loadURL(url);
+    
+    // cuando cambia la url , avisamos al renderer y guardamos en historial
+    view.webContents.on('did-navigate-in-page  ', (event, url) => {
+        const idx = views.findIndex(v => v.view === view);
+        if (idx !== -1 ){
+            views[idx].url = newUrl;   // actualiza URL interna
+            // notifica renderer con IPC 
+            mainWindow.webContents.send('tab-updated', {index.idx, url:newURL});
+            //guarda el historial en la DB 
+            DB.addHistory(newURL);
+        }
+});
