09202c2933a4bd1356bc5b7f0f263cc1496445ff
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-10-30 23:59:38 -0300

    codigo en renderer

---
+++ renderer/renderer.js
@@ -0,0 +1,6 @@
+
+// renderer.js  - ejecuta  en el  renderer (UI) . Usa navixAPI espuesto en preload.
+const address = document.getElementById('address'); // input  de dirreciones 
+const btnGo = document.getElementById('btn-go'); // boton IR
+const btnNew = document.getElementById('btn-new'); // boton nueva ventana
+const tabsDiv = document.getElementById('tabs'); // contenedor de pestañas
