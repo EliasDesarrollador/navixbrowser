9967ce75fda2a9fb72563af0c8a88e5c3c863a77
EliasDesarrollador <samuelortizamarilla@gmail.com>
2025-11-01 11:37:38 -0300

    funcion para renderizar las pestanas

--- renderer/renderer.js
+++ renderer/renderer.js
@@ -4,3 +4,14 @@ const address = document.getElementById('address'); // input  de dirreciones
 const btnGo = document.getElementById('btn-go'); // boton IR
 const btnNew = document.getElementById('btn-new'); // boton nueva ventana
 const tabsDiv = document.getElementById('tabs'); // contenedor de pestañas
+
+let tabs = [] ; // info local  de pestanas 
+
+// funcion para renderizar las pestanas  de  la UI  
+function renderTab(){
+    tabsDiv.innerHTML = ''; // limpiar contenedor
+    tabs.forEach(t => {
+        const btn = document.createElement('button');
+        
+    })
+}
